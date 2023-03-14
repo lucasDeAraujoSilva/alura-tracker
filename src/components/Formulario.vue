210f214a2c134637d5fa4615cfec5c5970abf2d2
lucasDeAraujoSilva <lucasaraujosilvamk@gmail.com>
2023-03-14 20:51:27 -0300

    adicionando funcionalidades e refatorando

---
+++ src/components/Formulario.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="box">
+    <div class="columns">
+      <div
+        class="column is-8"
+        role="form"
+        aria-label="formulário para criação de nova tarefa"
+      >
+        <input
+          type="text"
+          class="input"
+          placeholder="Qual tarefa você deseja inicar?"
+          v-model="descricao"
+        />
+      </div>
+      <div class="column">
+        <Temporizador @aoTemporizadorFinalizado="finalizarTarefa" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Temporizador from "./Temporizador.vue";
+
+export default defineComponent({
+  name: "Formulario",
+  components: {
+    Temporizador,
+  },
+  data() {
+    return {
+      descricao: "",
+    };
+  },
+  methods: {
+    finalizarTarefa(tempoDecorrido: number): void {
+      console.log("Tempo da tarefa:", tempoDecorrido);
+      console.log("Descrição da tarefa:", this.descricao);
+      this.descricao= ""
+    },
+  },
+});
+</script>
